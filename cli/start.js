7457710a796b7fac5f2e55edb7b5f20148a0e670
Paolo Insogna <paolo@cowtech.it>
2025-09-11 13:06:23 +0200

    chore: Removed leftover.
    Signed-off-by: Paolo Insogna <paolo@cowtech.it>

--- cli/start.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-
-import { printAndExitLoadConfigError } from '@platformatic/config'
-import { start } from '@platformatic/service'
-import { stackable } from '../lib/index.js'
-
-start(stackable, process.argv.splice(2)).catch(printAndExitLoadConfigError)
